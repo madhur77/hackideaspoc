658e03f29f4b71c032023e44d9530226e48c7925
Madhur <madhur.manocha@decathlon.com>
2021-06-14 05:16:52 +0530

    fix(listideas)

--- tests/components/ListIdeas/ListIdeas.test.js
+++ tests/components/ListIdeas/ListIdeas.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { getAllByTestId, screen } from "@testing-library/dom";
-import ListIdeas from "../../../src/pages/components/ListIdeas/ListIdeas";
+import { screen } from "@testing-library/dom";
+import ListIdeas from "../../../src/components/ListIdeas/ListIdeas";
 
 const mocklist = [
 	{
