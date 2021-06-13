5c0daa9a453ccf2a3d317a576b933ff93a279868
Madhur <madhur.manocha@decathlon.com>
2021-06-14 03:59:18 +0530

    test(add idea component)

---
+++ tests/components/Add/Add.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
+import AddIdeas from "../../../src/pages/components/Add/AddIdeas";
+
+describe("Add Idea component", () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+		render(<AddIdeas />);
+	});
+
+	it("TExtbox for title  is displayed", () => {
+		const textbox = screen.getByPlaceholderText("title");
+		expect(textbox).toBeDefined();
+	});
+	it("TExtbox for description is displayed", () => {
+		const textbox = screen.getByPlaceholderText("description");
+		expect(textbox).toBeDefined();
+	});
+
+	it("checkboxes are displayed and unchecked", () => {
+		const cb = screen.getAllByTestId("cb");
+		expect(cb).toBeDefined();
+	});
+});
