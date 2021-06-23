2ff497bcd9fb39fea2b0ba20349fa4486461bb3b
Madhur <madhur.manocha@decathlon.com>
2021-06-23 15:08:32 +0530

    adding auth control

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import Button from "../components/Button/Button"
 import TextBox from "../components/TextBox/TextBox"
-import { setCookie } from 'nookies';
+import nookies, { setCookie } from 'nookies';
 import router from 'next/router';
 
 
 const Login = () => {
-    const [user, SetUser] = useState<string>();
+    const [user, SetUser] = useState<string>(null);
+    const [error, setErrorMessage] = useState<string>("");
 
     const userCheck = (e) => { SetUser(e.target.value) };
 
     const loginHandler = async(e) => {
 
-        if (user == '') { alert("Please enter emp id"); return }
+        if (!user) {
+            alert("Please enter emp id");
+            setErrorMessage("Please input valid emp id");
+            return
+        }
         alert("Welcome to hack ideas");
         //save username to cookie
         setCookie(null, 'username', user, {
@@ -29,10 +34,36 @@ const Login = () => {
             <div className="text-black-600 text-2xl text-center px-8">HACK IDEAS LOGIN .<br/> Please enter your emp id!</div>
             <TextBox placeholder="EMP1001" changeHandler={userCheck} />
             <Button blabel="Enter arena!" clickHandler={loginHandler}></Button>
+            <div className="text-xl">{ error}</div>
             </div>
         </div>)
 
 }
+
+
+
+export const getServerSideProps = async(ctx) => {
+    
+    const cookies = nookies.get(ctx);
+    if (cookies.username) {
+        return {
+            redirect: {
+              permanent: false,
+              destination: '/home'
+            }
+        }
+       
+
+    }
+    return {
+        props: {}
+    }
+
+
+
+}
+
+
 
 
 export default Login;
